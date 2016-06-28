997113608f2132a0aeb0cd49503130634c3cb071
Huy Doan Quoc <huydq2@asiantech.vn>
2016-06-28 11:53:30 +0700

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('nick_name');
+            $table->string('image');
+            $table->tinyInteger('sex');
+            $table->dateTime('birthday');
+            $table->tinyInteger('member_type');
+            $table->string('facebook_id');
+            $table->string('twitter_id');
+            $table->string('device_token');
+            $table->tinyInteger('status');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
---
+++ database/migrations/2016_06_28_043957_create_admin_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('nick_name');
+            $table->string('image');
+            $table->tinyInteger('sex');
+            $table->dateTime('birthday');
+            $table->tinyInteger('roles');
+            $table->tinyInteger('status');
+            $table->rememberToken();
+            $table->timestamps();
+        });    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
